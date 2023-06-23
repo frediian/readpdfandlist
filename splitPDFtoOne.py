077a47132262ee00ae80296b2cd2f980b6c6bc7a
frediian <frediian@norbertdigital.com>
2023-06-22 23:15:55 -0500

    crear pdf por cada persona

--- splitPDFtoOne.py
+++ splitPDFtoOne.py
@@ -1,9 +1,20 @@
+from pypdf import PdfReader, PdfWriter
 import pandas as pd
 import sys
 
-archive= "./excel/"+str(sys.argv[1])+".xlsx"
+archiveExcel= "./excel/"+str(sys.argv[1])+".xlsx"
+archivePDF= "./pdfs/"+str(sys.argv[1])+".pdf"
 
-datos = pd.read_excel(archive)
-print (datos.shape[0])
+inputFile = PdfReader(archivePDF)
 
-print (datos[datos.columns[2]][0])
+datos = pd.read_excel(archiveExcel)
+nro_personas = datos.shape[0]
+
+for persona in range (nro_personas):
+    namepdf = "docs/ap_-_"+str(datos[datos.columns[2]][persona])+".pdf"
+    outputFile = PdfWriter()
+    outputFile.add_page(inputFile.pages[persona])
+    outputFile.write(namepdf)
+    outputFile.close()
+
+#print (datos[datos.columns[2]][0])
