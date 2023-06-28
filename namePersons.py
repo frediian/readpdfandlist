aedcf5c22071e4b5dcceef0f9ecd22151fdb6f51
frediian <fred@frediian.com>
2023-06-28 15:52:21 -0500

    obteniendo informacion de tabla

--- namePersons.py
+++ namePersons.py
@@ -4,9 +4,14 @@ import sys
 
 archiveExcel= "./excel/"+str(sys.argv[1])+".xls"
 
-datos = pd.read_excel(archiveExcel)
+datos = pd.read_excel(archiveExcel, dtype='str')
 
-print(len(datos.columns))
-print((datos.columns[0]))
-
+#print((datos.columns))
 find_data = "NÚMERO IDENTIFICACIÓN"
+persons=datos.get(find_data)
+numero=1085
+ced='1900855634'
+print(numero in datos[find_data])
+print(ced in datos[find_data].values)
+print(datos[find_data].keys())
+print( datos[find_data][numero])
