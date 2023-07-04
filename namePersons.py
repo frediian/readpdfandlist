ba11720ecac591d41ab10f3aebe3745b176dfb09
frediian <fred@frediian.com>
2023-07-04 09:38:59 -0500

    obteniendo nombres de la persona buscada

--- namePersons.py
+++ namePersons.py
@@ -1,23 +1,13 @@
 import pandas as pd
 import sys
 
-
 archiveExcel= "./excel/"+str(sys.argv[1])+".xls"
 
 datos = pd.read_excel(archiveExcel, dtype='str',index_col=None)
 
-#print((datos.columns))
-find_data = "NÚMERO IDENTIFICACIÓN"
-find_name = "NOMBRES"
+col_id = "NÚMERO IDENTIFICACIÓN"
+col_name = "NOMBRES"
 
-persons=datos.get(find_data)
-numero=1085
-ced='1900855634'
-df = pd.DataFrame(data=datos[find_name], index=datos[find_data])
-#df.set_index([find_data, find_name])
-print(df.index)
+person_id= str(sys.argv[2])
 
-print(df[find_name].get(ced))
-#print(numero in datos[find_data])
-#print(ced in datos[find_data].values)
-print( datos[find_data][numero])
+print(datos[datos[col_id]==person_id][col_name].values[0])
