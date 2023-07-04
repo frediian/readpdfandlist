b0a499697169130282539a89b577d25674deccde
frediian <frediian@norbertdigital.com>
2023-07-04 00:20:10 -0500

    test minors

--- namePersons.py
+++ namePersons.py
@@ -4,14 +4,20 @@ import sys
 
 archiveExcel= "./excel/"+str(sys.argv[1])+".xls"
 
-datos = pd.read_excel(archiveExcel, dtype='str')
+datos = pd.read_excel(archiveExcel, dtype='str',index_col=None)
 
 #print((datos.columns))
 find_data = "NÚMERO IDENTIFICACIÓN"
+find_name = "NOMBRES"
+
 persons=datos.get(find_data)
 numero=1085
 ced='1900855634'
-print(numero in datos[find_data])
-print(ced in datos[find_data].values)
-print(datos[find_data].keys())
+df = pd.DataFrame(data=datos[find_name], index=datos[find_data])
+#df.set_index([find_data, find_name])
+print(df.index)
+
+print(df[find_name].get(ced))
+#print(numero in datos[find_data])
+#print(ced in datos[find_data].values)
 print( datos[find_data][numero])
